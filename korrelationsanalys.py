148b3d2f9ce3ad93f0abd252c24ab4b89a30375f
André Sollander <andre@sollander.se>
2020-11-22 13:00:32 +0000

    Initial commit

---
+++ korrelationsanalys.py
@@ -0,0 +1,49 @@
+# ---------- Import (Rör ej här) ----------
+import numpy as np
+
+# ---------- Korrelationsfunction ----------
+def korrelationsfunction(x, y):
+  # Gör om till np matris
+  x = np.array(x)
+  y = np.array(y)
+  assert(len(x)==len(y))
+
+  # Antal värden
+  n = len(x)
+  print("n = " + str(n))
+
+  # Medelvärden
+  x_bar = np.mean(x)
+  print("μx = " + str(x_bar))
+
+  y_bar = np.mean(y)
+  print("μy = " + str(y_bar))
+
+  # Standardavikelser
+  x_std = np.std(x)
+  print("σx = " + str(x_std))
+
+  y_std = np.std(y)
+  print("σy = " + str(y_std) + "\n")
+
+  # Summor
+  x_sum = sum(x)
+  print("Σ(x) = " + str(x_sum))
+
+  y_sum = sum(y)
+  print("Σ(y) = " + str(y_sum))
+
+  xy_sum = x.dot(y)
+  print("Σ(xy) = " + str(xy_sum))
+
+  x2_sum = x.dot(x.T)
+  print("Σ(x^2) = " + str(x2_sum))
+
+  y2_sum = y.dot(y.T)
+  print("Σ(y^2) = " + str(xy_sum) + "\n")
+
+  # Korrelation
+  r = (n * xy_sum - x_sum * y_sum) / np.sqrt((n * x2_sum - x_sum**2)*(n * y2_sum - y_sum**2))
+  print("r = " + str(r))
+
+  return
---
+++ main.py
@@ -0,0 +1,15 @@
+# ---------- Import (Rör ej här) ----------
+from korrelationsanalys import korrelationsfunction
+
+# ---------- Lägg in data här ----------
+# Måste vara kommatecken (,) mellan varje värde
+# Tänk på att anväda punkt för decimaltal t.ex. (5.2 inte 5,2)
+
+# Skriv in x värderna här
+x = [225, 180, 345, 180, 210]
+
+# Skriv in y värderna här
+y = [6.7, 4.5, 11.3, 5.2, 7.8]
+
+# ---------- Korrelation (Rör ej här) ----------
+korrelationsfunction(x, y)
