02cbd31ee57a0ca09eb31567d6c4a1d3ea7cf799
André Sollander <andre@sollander.se>
2020-11-25 10:56:18 +0000

    Minor update

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ print("För att räkna Spearmans rangkorrelationskoefficient mata in rang")
 print("istället för värden.")
 print("\nTänk på att anväda punkt för decimaltal och inte kommatecken t.ex. (5.2 inte 5,2).")
 
-
 # ---------- Läs in data ----------
 x, y = datahantering()
 
